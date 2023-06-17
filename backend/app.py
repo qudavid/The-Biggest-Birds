870720a7f64955050b415e446666db0d666c3ed3
Jeff Liu <jeffliu@Jeffs-MacBook-Pro.local>
2023-06-17 12:36:41 -0700

    backend

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,9 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import os
 import openai
 
 openai.organization = "org-OcpyA7VqpEkcFM2BWDVDBff3"
-openai.api_key = "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"
+openai.api_key = ""
 
 app = Flask(__name__)
 
@@ -15,14 +15,23 @@ def index():
 @app.route('/message', methods=['GET'])
 def get_prompt_from_user():
     message = request.args.get('message')
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=message,
-        max_tokens=100
-    )
-    gpt4_response = response.choices[0].text.strip()
+    if message:
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=message,
+            max_tokens=100
+        )
+        gpt4_response = response.choices[0].text.strip()
 
-    return gpt4_response
+        return gpt4_response
+    else:
+        return "No response received."
+    
+
+@app.route('/prompt', methods=['POST'])
+def get_prompt():
+    user_message = request.form['message']
+    return render_template('message.html', message=user_message)
 
 
 if __name__ == "__main__":

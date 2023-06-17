ba29870e6f406c1dd492d7d6a78d9ae601f016d2
Jeff Liu <jeffliu@Jeffs-MacBook-Pro.local>
2023-06-17 12:04:09 -0700

    backend openai

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,21 @@
 from flask import Flask, request
+import os
+import openai
+
+openai.organization = "org-OcpyA7VqpEkcFM2BWDVDBff3"
+openai.api_key = os.getenv("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")
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    pass
+    return "Hello"
 
 @app.route('/message', methods=['GET'])
 def get_prompt_from_user():
     message = request.args.get('message')
-    
+    return message
 
 
 if __name__ == "__main__":

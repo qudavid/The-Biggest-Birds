98b4d7e455473fa047fa2843bfa6f0e3fc3575ff
Alena Chao <alenachao@berkeley.edu>
2023-06-18 03:26:24 -0700

    logo!

--- frontend/src/components/Create/Create.jsx
+++ frontend/src/components/Create/Create.jsx
@@ -8,9 +8,14 @@ import Form from '../Form/Form';
 const Create = () => (
   <>
     <Container maxWidth="xl">
-      <Grid container spacing={3} paddingTop="100px">
+      <Grid container spacing={3} padding="20px">
         <Grid item xs={12} md={6} lg={12}>
-          <Card style={{ maxWidth: 800, padding: '20px 5px', margin: '0 auto' }}>
+          <Typography variant="h2" color="textPrimary" textAlign="center" gutterBottom>
+                Be real, be moody.
+          </Typography>
+        </Grid>
+        <Grid item xs={12} md={6} lg={12}>
+          <Card style={{ maxWidth: 800, margin: '0 auto' }}>
             <CardContent>
               <Typography variant="h6" color="textPrimary" component="p" textAlign="center" gutterBottom>
                 Describe your day and we will generate an abstract art of your mood.
--- frontend/src/components/Form/Form.jsx
+++ frontend/src/components/Form/Form.jsx
@@ -25,7 +25,7 @@ const Form = () => {
     <form onSubmit={handleSubmit}>
       <Grid container spacing={1}>
         <Grid item xs={12}>
-          <TextField onChange={handleInput} label="How was your day" multiline rows={20} placeholder="Type your message here" variant="outlined" fullWidth required />
+          <TextField onChange={handleInput} label="How was your day?" multiline rows={20} placeholder="Type your message here" variant="outlined" fullWidth required />
         </Grid>
         <Grid item xs={12}>
           <Button type="generate" variant="contained" color="primary" fullWidth>Submit</Button>

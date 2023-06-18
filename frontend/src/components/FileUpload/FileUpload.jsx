41e1216f675769794ca5166d99f7ec0dcdedead3
Larry Pang <larrypang@Larrys-MacBook-Pro.local>
2023-06-17 23:36:41 -0700

    uploadfile

--- frontend/src/components/FileUpload/FileUpload.jsx
+++ frontend/src/components/FileUpload/FileUpload.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import axios from 'axios';
 
-const FileUpload = () => (
-  <div>FileUpload</div>
-);
-
+const FileUpload = () => {
+  const handleFileUpload = (event) => {
+    // get the selected file from the input
+    const file = event.target.files[0];
+    // create a new FormData object and append the file to it
+    const formData = new FormData();
+    formData.append('file', file);
+    // make a POST request to the File Upload API with the FormData object and Rapid API headers
+    axios
+      .post('http://127.0.0.1:5000//upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+      .then((response) => {
+        // handle the response
+        console.log(response);
+      })
+      .catch((error) => {
+        // handle errors
+        console.log(error);
+      });
+  };
+  // render a simple input element with an onChange event listener that calls the handleFileUpload function
+  return (
+    <div>
+      <input type="file" onChange={handleFileUpload} />
+    </div>
+  );
+};
 export default FileUpload;
